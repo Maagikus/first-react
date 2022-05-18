4391252a444156817852615cbf8fa398cf901135
Yan <maagikus@gmail.com>
2022-05-18 13:21:04 +0300

    learn jsx firs lesson

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const text = 'Hello World';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const elem = (
+  <div>
+    <h2 className='text'>{text}</h2>
+    <input type="text" name="" id="" />
+    <button>Click</button>
+  </div>
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+elem,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
